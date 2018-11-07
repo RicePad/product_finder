c87e269219393eb29eba96b4106279cf298125d0
JON <jonn.thinkrtc@gmail.com>
2018-11-07 10:54:44 -0500

    static root

--- productfinder/settings.py
+++ productfinder/settings.py
@@ -15,7 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, "templates")
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_DIRECTORY = os.path.join(BASE_DIR, "static")
 MEDIA_DIRECTORY = os.path.join(BASE_DIR, "media")
 
 
@@ -130,12 +130,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
 
-]
 STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIR = [
+    STATIC_DIRECTORY,
+]
 
 
 MEDIA_ROOT = MEDIA_DIRECTORY
